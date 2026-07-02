5509baf4a9624f0bfa57ad2f5e4ba74d1a215098
icphcourse <ilyassovassel@icloud.com>
2026-07-02 13:34:57 +0500

    Add files via upload

---
+++ story_content/5YYPnAbUKFH_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5YYPnAbUKFH_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.118.37034.0%0D%0A%0D%0A00%3A00%3A00.075%20--%3E%2000%3A00%3A01.875%0D%0AWelcome%20to%20Closing%20%26%20Commitments%21%0D%0A%0D%0A"}]})
